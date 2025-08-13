2e34a4c7754244ccae428c9b65f1e20e59e1f64d
jinnosukeKato <jinnosukeKato@gmail.com>
2025-08-13 23:03:13 +0900

    不要なregを削除

--- src/cpu_test.v
+++ src/cpu_test.v
@@ -1,11 +1,11 @@
 `default_nettype none
 
 module tt_um_cpu_test(
-    input  reg [7:0] ui_in,    // Dedicated inputs
+    input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  reg [7:0] uio_in,   // IOs: Input path
-    output reg [7:0] uio_out,  // IOs: Output path
-    output reg [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
@@ -17,13 +17,13 @@ module tt_um_cpu_test(
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, rst_n, 1'b0};
 
-  reg [3:0] registerA;
-  reg [3:0] registerB;
-  reg [7:0] registerOut;
+  wire [3:0] registerA;
+  wire [3:0] registerB;
+  wire [7:0] registerOut;
   wire carry;
   wire [3:0] pc_out;
 
-  reg [3:0] opcode;
+  wire [3:0] opcode;
   wire [3:0] immediate;
 
   assign opcode = ui_in[3:0];
