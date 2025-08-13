3aef1e8dda8e984d3c805494521036bf6480469d
jinnosukeKato <jinnosukeKato@gmail.com>
2025-08-13 23:03:40 +0900

    1命令ごとにNOPを挟んで正しくassertできるようにした

--- test/test_cpu.py
+++ test/test_cpu.py
@@ -17,17 +17,26 @@ async def test_project(dut):
   dut.rst_n.value = 0
   await ClockCycles(dut.clk, 10)
   dut.rst_n.value = 1
+  await ClockCycles(dut.clk, 1)
 
   dut._log.info("Test project behavior")
 
   # Test
   # ADD A, Im (Op:0000, Im:1010)
-  dut.ui_in.value = 0b0101_0000 
-  await ClockCycles(dut.clk, 2)
+  dut._log.info("add A, Im")
+  dut.ui_in.value = 0b0101_0000
+  await ClockCycles(dut.clk, 1)
+  dut.ui_in.value = 0b0000_0000
+  await ClockCycles(dut.clk, 1) # NOPで出力が立ち上がるのを待ってからassert
   assert dut.uo_out.value == 0b0000_0101
 
-  # TODO!!! 1サイクルで計算が回り切っていない
   # ADD B, Im (Op:0101, Im:1010)
+  dut._log.info("add B, Im")
   dut.ui_in.value = 0b0101_1010
-  await ClockCycles(dut.clk, 2)
-  assert dut.uo_out.value == 0b0101_1010
+  await ClockCycles(dut.clk, 1)
+  dut.ui_in.value = 0b0000_0000
+  await ClockCycles(dut.clk, 1) # NOPで出力が立ち上がるのを待ってからassert
+  assert dut.uo_out.value == 0b0101_0101
+
+  dut._log.info("Test finished")
+
